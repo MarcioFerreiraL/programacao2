2b6b82a28edd0ce95d0f49148d34965a33159232
Márcio Ferreira Lima <contactmarcioflima@gmail.com>
2025-03-15 21:13:57 -0300

    Aula 01

---
+++ ProgramacaoII/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        double peso, altura, imc;
+
+        peso = 80;
+        altura = 1.73;
+
+        imc = peso / (altura*altura);
+
+        System.out.println("1 - Adição");
+        System.out.println("2 - Subtração");
+        System.out.println("Digite a opção desejada");
+
+        int op = 1;
+
+        }
+    }
